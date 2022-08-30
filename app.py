0d6b6310c7a0d9943a4222167da2d4b3adef8270
DeepikaNelson <105265596+DeepikaNelson@users.noreply.github.com>
2022-08-30 15:54:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, This is CI CD Pipeline using Jenkins and GitOps (ArgoCD) to dockerize your code, and deploy the container into a Kubernetes cluster.'
+    echo 'thank you'
